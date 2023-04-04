67952c2598fb6b348bd6b132fca4282d06149cfc
gtiezzi <gianni.tiezzi@mercadolibre.com>
2023-04-04 10:39:53 -0300

    Change img for button.

--- js/content.js
+++ js/content.js
@@ -1,4 +1,4 @@
-const getButtons = (name = null) => document.querySelectorAll(name ? `img[data-emoji=${name}]` : 'img[data-emoji]');
+const getButtons = (name = null) => document.querySelectorAll(name ? `button[data-emoji=${name}]` : 'button[data-emoji]');
 const getMoodButton = () => [...document.querySelectorAll('i')].find(el => el.textContent === 'mood');
 
 chrome.runtime.onMessage.addListener(function (response, sender, sendResponse) {
